4d72754a73c789d9d3df7932111baf4499c0a8d1
Angelica Barreda B. <anjelyk.b@gmail.com>
2015-02-17 11:07:18 -0600

    add

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<base href="<?php echo base_url(); ?>">
 	<meta charset="UTF-8">
 	<title>Document</title>
+ 	<!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 <body>
 	<?php
@@ -16,5 +26,10 @@
 	<a href="index.php/prueba/opcion3">Opcion 3</a>
 	<?php
 	?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
